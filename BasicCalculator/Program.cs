b8231ab705a801c22a54edab269a75aca0750845
neilquizon <neil.quizon@gmail.com>
2019-10-30 14:50:16 -0700

    Add project files.

---
+++ BasicCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name of the app
+
+            Console.Write("\n\n");
+            Console.Write("Basic calculator:\n");
+            Console.Write("------------------------------------------------");
+            Console.Write("\n\n");
+        }
+    }
+}
