21eee179b7458b11cba007f873fd3ff1bcabe19c
neilquizon <neil.quizon@gmail.com>
2019-10-30 15:24:38 -0700

    #1 Made some changes

--- BasicCalculator/Program.cs
+++ BasicCalculator/Program.cs
@@ -15,22 +15,19 @@ namespace BasicCalculator
 
             //Choose math operator
 
-            Console.Write("\nPlease choose from the below options :\n");
-            Console.Write("1-Addition.\n2-Substraction.\n3-Multiplication.\n4-Division.\n5-Remainder.\n6-Exit.\n");
-            Console.Write("\nInput your choice :");
-
-            // Choose First and Second Number
-
             int num1, num2, opt;
+
+            Console.Write("\nPlease choose from the below options :\n\n");
+            Console.Write("1-Addition.\n2-Substraction.\n3-Multiplication.\n4-Division.\n5-Remainder.\n6-Exit.\n");
+            Console.Write("\nPLease enter your choice :");
+            opt = Convert.ToInt32(Console.ReadLine());
+
+            // Choose First and Second Number            
 
             Console.Write("Enter First Number :");
             num1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter Second Number :");
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-            // Math operator
-
-            opt = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine()); 
 
             // Case statement
 
@@ -38,7 +35,9 @@ namespace BasicCalculator
             {
                 case 1:
                     Console.Write("The Sum of  {0} and {1} is: {2}\n", num1, num2, num1 + num2);
+                    Console.Read();
                     break;
+                    
 
 
             }
