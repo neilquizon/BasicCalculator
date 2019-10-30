f35223e7c4a030c412b6ba623f4a476f33f77cc5
neilquizon <neil.quizon@gmail.com>
2019-10-30 15:27:44 -0700

    #1 added subtraction and made some changes

--- BasicCalculator/Program.cs
+++ BasicCalculator/Program.cs
@@ -19,14 +19,14 @@ namespace BasicCalculator
 
             Console.Write("\nPlease choose from the below options :\n\n");
             Console.Write("1-Addition.\n2-Substraction.\n3-Multiplication.\n4-Division.\n5-Remainder.\n6-Exit.\n");
-            Console.Write("\nPLease enter your choice :");
+            Console.Write("\nPLease enter your choice: ");
             opt = Convert.ToInt32(Console.ReadLine());
 
             // Choose First and Second Number            
 
-            Console.Write("Enter First Number :");
+            Console.Write("Enter First Number: ");
             num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Second Number :");
+            Console.Write("Enter Second Number: ");
             num2 = Convert.ToInt32(Console.ReadLine()); 
 
             // Case statement
@@ -34,10 +34,16 @@ namespace BasicCalculator
             switch (opt)
             {
                 case 1:
-                    Console.Write("The Sum of  {0} and {1} is: {2}\n", num1, num2, num1 + num2);
+                    Console.Write("The Sum of {0} and {1} is: {2}\n", num1, num2, num1 + num2);
                     Console.Read();
                     break;
-                    
+
+                case 2:
+                    Console.Write("The Difference of {0} and {1} is: {2}\n", num1, num2, num1 - num2);
+                    Console.Read();
+                    break;
+
+
 
 
             }
