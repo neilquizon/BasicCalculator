9ec8ac1d6a07f37dfbbc0573952d6bbf1f87cf6f
neilquizon <neil.quizon@gmail.com>
2019-10-30 14:54:24 -0700

    #1 Added: Choose math operator

--- BasicCalculator/Program.cs
+++ BasicCalculator/Program.cs
@@ -12,6 +12,12 @@ namespace BasicCalculator
             Console.Write("Basic calculator:\n");
             Console.Write("------------------------------------------------");
             Console.Write("\n\n");
+
+            //Choose math operator
+
+            Console.Write("\nHere are the options :\n");
+            Console.Write("1-Addition.\n2-Substraction.\n3-Multiplication.\n4-Division.\n5-Exit.\n");
+            Console.Write("\nInput your choice :");
         }
     }
 }
