dff06b8af5e838f1c7f3a2b4636f7d81e2d87a11
neilquizon <neil.quizon@gmail.com>
2019-10-30 18:19:06 -0700

    fixes #1 finalizing

--- BasicCalculator/Program.cs
+++ BasicCalculator/Program.cs
@@ -15,22 +15,36 @@ namespace BasicCalculator
             Console.Write("\n\n");
 
             //Choose math operator
-            int num1, num2, opt;
+            decimal num1, num2, opt;
 
             // Choose First and Second Number         
 
 
+
             Console.Write("Enter First Number: ");
-            num1 = Convert.ToInt32(Console.ReadLine());
+            
+            while (!decimal.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.WriteLine("That was invalid. Enter a valid Number"); 
+            }           
+                       
             Console.Write("Enter Second Number: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
+
+            while (!decimal.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.WriteLine("That was invalid. Enter a valid Number");
+            }
+           
 
 
 
             Console.Write("\nPlease choose from the below options :\n\n");
             Console.Write("1-Addition.\n2-Substraction.\n3-Multiplication.\n4-Division.\n5-Remainder.\n6-Exit.\n");
             Console.Write("\nPLease enter your choice: ");
-            opt = Convert.ToInt32(Console.ReadLine());
+            while (!decimal.TryParse(Console.ReadLine(), out opt))
+            {
+                Console.WriteLine("That was invalid. Enter a valid Number");
+            }           
 
             
 
@@ -77,6 +91,7 @@ namespace BasicCalculator
 
                 //exit
                 case 6:
+
                     Console.Write("\nGoodbye!!!\n");
                     Console.Read();
                     break;
