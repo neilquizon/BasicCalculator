012fcecacb4ac151b9c47f9fc2b32ede42794ae4
neilquizon <neil.quizon@gmail.com>
2019-11-13 10:36:22 -0800

    #1 Added Round function

--- BasicCalculator/Program.cs
+++ BasicCalculator/Program.cs
@@ -35,10 +35,7 @@ namespace BasicCalculator
                 Console.WriteLine("That was invalid. Enter a valid Number");
             }
            
-
-
-
-            Console.Write("\nPlease choose from the below options :\n\n");
+                                 Console.Write("\nPlease choose from the below options :\n\n");
             Console.Write("1-Addition.\n2-Substraction.\n3-Multiplication.\n4-Division.\n5-Remainder.\n6-Exit.\n");
             Console.Write("\nPLease enter your choice: ");
             while (!decimal.TryParse(Console.ReadLine(), out opt))
@@ -54,19 +51,19 @@ namespace BasicCalculator
             {
                 //addition
                 case 1:               
-                    Console.Write("\nThe Sum of {0} and {1} is: {2}\n", num1, num2, num1 + num2);
+                    Console.Write("\nThe Sum of {0} and {1} is: {2}\n", num1, num2, Math.Round(num1 + num2, 2));
                     Console.Read();
                     break;
 
                 //subtraction
                 case 2:
-                    Console.Write("\nThe Difference of {0} and {1} is: {2}\n", num1, num2, num1 - num2);
+                    Console.Write("\nThe Difference of {0} and {1} is: {2}\n", num1, num2, Math.Round(num1 - num2, 2));
                     Console.Read();
                     break;
 
                 //multiplication
                 case 3:
-                    Console.Write("\nThe Product of {0} and {1} is: {2}\n", num1, num2, num1 * num2);
+                    Console.Write("\nThe Product of {0} and {1} is: {2}\n", num1, num2, Math.Round(num1 * num2, 2));
                     Console.Read();
                     break;
 
@@ -78,14 +75,14 @@ namespace BasicCalculator
                     }
                     else
                     {
-                        Console.Write("\nThe Quotient of {0} and {1} is : {2}\n", num1, num2, num1 / num2);
+                        Console.Write("\nThe Quotient of {0} and {1} is : {2}\n", num1, num2, Math.Round(num1 / num2, 2));
                     }
                     Console.Read();
                     break;
 
                 //Remainder
                 case 5:
-                    Console.Write("\nThe Remainder of {0} and {1} is: {2}\n", num1, num2, num1 % num2);
+                    Console.Write("\nThe Remainder of {0} and {1} is: {2}\n", num1, num2, Math.Round(num1 % num2, 2));
                     Console.Read();
                     break;
 
